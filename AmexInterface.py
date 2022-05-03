be6fdc92666aabf0f5dd0822ef25819bc115a3a5
elias-a <elias.alstead@gmail.com>
2022-05-03 19:34:50 -0400

    Refresh page until no more offers are found

--- AmexInterface.py
+++ AmexInterface.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class AmexInterface():
     def __init__(self, driver):
@@ -21,9 +22,17 @@ class AmexInterface():
         loginButton.click()
 
     def addOffers(self):
-        buttons = WebDriverWait(self.driver, 6).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//*[contains(text(), 'Add to Card')]"))
-        )
-        
-        for button in buttons:
-            self.driver.execute_script("arguments[0].click();", button)
+        while True:
+            try:
+                buttons = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_all_elements_located(
+                        (By.XPATH, "//*[contains(text(), 'Add to Card')]")
+                    )
+                )
+            except TimeoutException:
+                break
+
+            for button in buttons:
+                self.driver.execute_script("arguments[0].click();", button)
+
+            self.driver.refresh()
