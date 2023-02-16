204e198081cd3dcc64a69aa68838b7c4e09d8599
elias-a <elias.alstead@gmail.com>
2023-02-16 18:17:03 -0500

    Write the output of the program to a log file

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import logging
 import pathlib
 from configparser import ConfigParser
 from ChromeDriver import ChromeDriver
 from AmexInterface import AmexInterface
+
+logging.basicConfig(
+    filename="amex-offers.log",
+    encoding="utf-8",
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logging.info("Starting program to add Amex offers...")
 
 config = ConfigParser()
 configPath = f"{pathlib.Path(__file__).parent.resolve()}/config.ini"
@@ -9,11 +20,15 @@ config.read(configPath)
 username = config["AMEX"]["username"]
 password = config["AMEX"]["password"]
 
+logging.info("Opening Chrome...")
+driver = ChromeDriver(configPath)
 try:
-    driver = ChromeDriver(configPath)
-
     amexInterface = AmexInterface(driver.driver)
     amexInterface.authenticate(username, password)
     amexInterface.addOffers()
+except Exception as e:
+    logging.error(e)
 finally:
+    logging.info("Closing Chrome...")
     driver.closeChrome()
+
