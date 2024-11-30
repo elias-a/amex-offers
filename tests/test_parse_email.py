080544bb82bfd6526a2fdc6afb1a7e4ee48737de
elias-a <elias.alstead@gmail.com>
2024-11-30 17:05:55 -0500

    Test parsing verification email to extract code

---
+++ tests/test_parse_email.py
@@ -0,0 +1,14 @@
+import os
+from unittest import TestCase
+from bs4 import BeautifulSoup
+
+
+class Test_parse_email(TestCase):
+    def test_parse_email(self):
+        path = os.path.join(os.path.dirname(__file__), "data/email.html")
+        with open(path, "rt") as f:
+            message = f.read()
+        soup = BeautifulSoup(message, "html.parser")
+        previous_sibling = soup.find("tr", text="Your Re-authentication Key:")
+        code = previous_sibling.find_next_sibling().text
+        assert code == "896308"
